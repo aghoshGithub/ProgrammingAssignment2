436b5915a9cf15038223970efd8646fee1969081
aghoshGithub <abh_gho@yahoo.com>
2015-10-25 00:52:09 -0700

    for Progass2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions are defined to create a special object that
+## stores a matrix and caches its inverse
 
-## Write a short comment describing this function
+## A matrix object is created which can cache its inverse by this function
 
 makeCacheMatrix <- function(x = matrix()) {
+Inv <- NULL
 
+setIt <- function(a) {
+  x <<- a
+  Inv <<- NULL
+}
+getIt <- function() x
+setInv <- function(Inverse) inv <<- inverse
+getInv <- function() inv
+list(setIt = setIt,
+     getIt = getIt,
+     setInv = setInv,
+     getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+
+## THe ivnerse of "matrix" is computed by this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  Inv <- x$getInv()
+  if(!is.null(Inv)) {
+    return(Inv)
+  }
+  matr <- x$get()
+  Inv <- solve(matr, ...)
+  x$setInv(inv)
+  Inv
 }
